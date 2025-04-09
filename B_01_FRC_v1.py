bd78a8a295b7535d4d9262ffcbb3951c694d5d5a
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2025-04-10 10:00:42 +1200

    Add files via upload

---
+++ B_01_FRC_v1.py
@@ -0,0 +1,233 @@
+import pandas
+from tabulate import tabulate
+from datetime import date
+
+
+# Functions
+def make_statement(statement, decoration):
+    """Emphasises heading by adding decoration
+    at the start and end"""
+
+    return f"{decoration * 3} {statement} {decoration * 3}\n"
+
+
+def yes_no_check(question):
+    """Checks that users enter yes / y or no / n to a question"""
+
+    while True:
+
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes (y) or no (no).\n")
+
+
+def instructions():
+    """Displays instructions"""
+    print(make_statement("Instructions", "ℹ️"))
+
+    print('''This program will ask you for... 
+    - The name of the product you are selling 
+    - How many items you plan on selling 
+    - The costs for each component of the product 
+      (variable expenses)
+    - Whether or not you have fixed expenses (if you have 
+      fixed expenses, it will ask you what they are).
+    - How much money you want to make (ie: your profit goal)
+
+It will also ask you how much the recommended sales price should 
+be rounded to.
+
+The program outputs an itemised list of the variable and fixed 
+expenses (which includes the subtotals for these expenses). 
+
+Finally it will tell you how much you should sell each item for 
+to reach your profit goal. 
+
+The data will also be written to a text file which has the 
+same name as your product and today's date.
+
+    ''')
+
+
+def not_blank(question):
+    """Checks user response is not blank"""
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry, this can't be blank.")
+
+
+def num_check(question, num_type="float", exit_code=None):
+    """Checks that response is a float / integer more than zero"""
+
+    if num_type == "float":
+        error = "Please enter a number more than 0."
+    else:
+        error = "Please enter an integer more than 0."
+
+    while True:
+
+        response = input(question)
+
+        # check for exit code and return it if entered
+        if response == exit_code:
+            return response
+
+        # check datatype is correct and that number
+        # is more than zero
+        try:
+
+            if num_type == "float":
+                response = float(response)
+            else:
+                response = int(response)
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+def get_expenses(exp_type, how_many=1):
+    """Gets variable / fixed expenses and outputs
+    panda (as a string) and a subtotal of the expenses"""
+
+    # Lists for panda
+    all_items = []
+    all_amounts = []
+    all_dollar_per_item = []
+
+    # Expenses dictionary
+    expenses_dict = {
+        "Item": all_items,
+        "Amount": all_amounts,
+        "$ / Item": all_dollar_per_item
+    }
+
+    # defaults for fixed expenses
+    amount = how_many   # how_many defaults to 1
+    how_much_question = "How much? $"
+
+    # loop to get expenses
+    while True:
+
+        # Get item name and check it's not blank
+        item_name = not_blank("Item Name: ")
+
+        # check users enter at least one variable expense
+        if exp_type == "variable" and item_name == "xxx" and len(all_items) == 0:
+            print("Oops - you have not entered anything.  "
+                  "You need at least one item.")
+            continue
+
+        # end loop when users enter exit code
+        elif item_name == "xxx":
+            break
+
+        # Get variable expenses item amount <enter> defaults to number of
+        # products being made.
+        if exp_type == "variable":
+
+            amount = num_check(f"How many <enter for {how_many}>: ",
+                               "integer", "")
+
+            # Allow users to push <enter> to default to number of items being made
+            if amount == "":
+                amount = how_many
+
+            how_much_question = "Price for one? $"
+
+        # Get price for item (question customised depending on expense type).
+        price_for_one = num_check(how_much_question, "float")
+
+        all_items.append(item_name)
+        all_amounts.append(amount)
+        all_dollar_per_item.append(price_for_one)
+
+    # make panda
+    expense_frame = pandas.DataFrame(expenses_dict)
+
+    # Calculate Cost Column
+    expense_frame['Cost'] = expense_frame['Amount'] * expense_frame['$ / Item']
+
+    # calculate subtotal
+    subtotal = expense_frame['Cost'].sum()
+
+    # Apply currency formatting to currency columns.
+    add_dollars = ['Amount', '$ / Item', 'Cost']
+    for var_item in add_dollars:
+        expense_frame[var_item] = expense_frame[var_item].apply(currency)
+
+    # make expense frame into a string with the desired columns
+    if exp_type == "variable":
+        expense_string = tabulate(expense_frame, headers='keys',
+                                  tablefmt='psql', showindex=False)
+    else:
+        expense_string = tabulate(expense_frame[['Item', 'Cost']], headers='keys',
+                                  tablefmt='psql', showindex=False)
+
+    # return the expenses panda and subtotal
+    return expense_string, subtotal
+
+
+def currency(x):
+    """Formats numbers as currency ($#.##)"""
+    return "${:.2f}".format(x)
+
+
+# Main routine goes here
+
+# initialise variables...
+
+# assume we have no fixed expenses for now
+fixed_subtotal = 0
+fixed_panda_string = ""
+
+print(make_statement("Fund Raising Calculator", "💰"))
+
+print()
+want_instructions = yes_no_check("Do you want to see the instructions? ")
+print()
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+# Get product details...
+product_name = not_blank("Product Name: ")
+quantity_made = num_check("Quantity being made: ", "integer")
+
+# Get variable expenses...
+print("Let's get the variable expenses....")
+variable_expenses = get_expenses("variable", quantity_made)
+
+variable_panda = variable_expenses[0]
+variable_subtotal = variable_expenses[1]
+
+# ask user if they have fixed expenses and retrieve them
+print()
+has_fixed = yes_no_check("Do you have fixed expenses? ")
+
+if has_fixed == "yes":
+    fixed_expenses = get_expenses("fixed")
+
+    variable_panda = variable_expenses[0]
+    variable_subtotal = variable_expenses[1]
+
+    # If the user has not entered any fixed expenses,
+    # # Set empty panda to "" so that it does not display!
+    if fixed_subtotal == 0:
+        has_fixed = "no"
+        fixed_panda_string = ""
